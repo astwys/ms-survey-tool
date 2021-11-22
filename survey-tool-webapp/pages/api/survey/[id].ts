9026f153f0a6e8175c96cf54d7e2b5c231a971d1
Michael Bartl <astwys@gmail.com>
2021-11-22 09:32:41 +0100

    (webapp) add findAll, findById, createOne routes

---
+++ survey-tool-webapp/pages/api/survey/[id].ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Survey } from '../../../types/Survey'
+import { findById } from '../../../mockApi'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Survey | string>
+) {
+  const { id } = req.query
+  const idNumber = parseInt(id as string)
+  const survey = findById(idNumber)
+  if (!survey) {
+    res.status(404).send('Not Found')
+    return
+  }
+  res.status(200).json(survey)
+}
---
+++ survey-tool-webapp/pages/api/survey/index.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Survey } from '../../../types/Survey'
+import { findAll, createOne } from '../../../mockApi'
+
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Survey | Survey[] | string>
+) {
+  if (req.method === 'GET') {
+    const surveys = findAll()
+    res.status(200).json(surveys)
+  } else if (req.method === 'POST') {
+    const survey = createOne(req.body)
+    res.status(200).json(survey)
+  } else {
+    res.status(501).send('Not Implemented')
+  }
+}
