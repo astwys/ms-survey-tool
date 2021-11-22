3f692be9e1f0be20a37627443689299799027c7e
karobaumgartner <astwys@gmail.com>
2021-11-22 18:50:43 +0100

    (webapp) add authentication to api endpoints

--- survey-tool-webapp/pages/api/survey/[id].ts
+++ survey-tool-webapp/pages/api/survey/[id].ts
@@ -1,9 +1,13 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import dbConnect from '../../../lib/dbConnect'
+import { sessionOptions } from '../../../lib/session'
 import { findById, updateById } from '../../../mongoose/persistence/Survey'
 import { Survey } from '../../../types/Survey'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Survey | string>) {
+export default withIronSessionApiRoute(handler, sessionOptions)
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Survey | string>) {
   const { id } = req.query
 
   await dbConnect()
@@ -15,6 +19,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     }
     res.status(200).json(survey)
   } else if (req.method === 'PUT') {
+    if (!req.session.user) {
+      res.status(401).send('Unauthorized')
+      return
+    }
+
     const survey = await updateById(id as string, req.body)
     if (!survey) {
       res.status(404).send('Not Found')
--- survey-tool-webapp/pages/api/survey/index.ts
+++ survey-tool-webapp/pages/api/survey/index.ts
@@ -2,16 +2,22 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { Survey } from '../../../types/Survey'
 import dbConnect from '../../../lib/dbConnect'
 import { createOne, findAll } from '../../../mongoose/persistence/Survey'
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { sessionOptions } from '../../../lib/session'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Survey | Survey[] | string>,
-) {
+export default withIronSessionApiRoute(handler, sessionOptions)
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Survey | Survey[] | string>) {
   await dbConnect()
   if (req.method === 'GET') {
     const surveys = await findAll()
     res.status(200).json(surveys)
   } else if (req.method === 'POST') {
+    if (!req.session.user) {
+      res.status(401).send('Unauthorized')
+      return
+    }
+
     const survey = await createOne(req.body)
     if (!survey) {
       res.status(400).send("Couldn't create survey.")
