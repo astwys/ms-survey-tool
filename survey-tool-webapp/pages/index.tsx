f0ba1fb3b7ec25013bf8cc508c8972a98314afaf
karobaumgartner <astwys@gmail.com>
2021-11-23 08:40:51 +0100

    (webapp) add more info text on index page

--- survey-tool-webapp/pages/index.tsx
+++ survey-tool-webapp/pages/index.tsx
@@ -1,12 +1,37 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import Layout from '../components/templates/Layout'
-import styles from '../styles/Home.module.css'
+import useUser from '../lib/useUser'
 
 const Home: NextPage = () => {
+  const { user } = useUser()
+
+  const renderLoggedInInfo = () => (
+    <p>
+      Welcome! Check out the{' '}
+      <Link href="/dashboard">
+        <a>Dashboard</a>
+      </Link>{' '}
+      to see all surveys
+    </p>
+  )
+  const renderLoggedOutInfo = () => (
+    <p>
+      Please{' '}
+      <Link href="/login">
+        <a>log in</a>
+      </Link>{' '}
+      to access the Survey Tool
+    </p>
+  )
+
   return (
     <Layout>
       <div className="container">
-        <main></main>
+        <main>
+          <h1>Survey Tool</h1>
+          {user?.isLoggedIn ? renderLoggedInInfo() : renderLoggedOutInfo()}
+        </main>
       </div>
     </Layout>
   )
