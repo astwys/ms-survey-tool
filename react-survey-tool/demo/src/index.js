85ac4a2688810fee1b31fe30bbc9026657e760d3
karobaumgartner <astwys@gmail.com>
2021-11-23 09:01:03 +0100

    (lib) add survey token implementation to demo

--- react-survey-tool/demo/src/index.js
+++ react-survey-tool/demo/src/index.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 
 import SurveyTool from '../../src'
+import surveyToken from './.env'
 
 export default class Demo extends Component {
   render() {
     return (
       <div>
         <h1>react-survey-tool Demo</h1>
-        <SurveyTool surveyLink="http://localhost:3000/survey/1" />
+        <SurveyTool
+          surveyLink={`http://localhost:3000/survey/619c9c04583e7d8cc97c860d?token=${surveyToken}`}
+        />
       </div>
     )
   }
